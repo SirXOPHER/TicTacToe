5ab06b24dac6ca906e4c6a8130c68125ddd7782d
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-05 11:37:32 +0100

    Refactoring of test suite, extracted setUp

--- src/test/java/TicTacToeShould.java
+++ src/test/java/TicTacToeShould.java
@@ -1,17 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TicTacToeShould {
 
+    TicTacToe ticTacToe;
+
+    @Before
+    public void setUp() {
+        ticTacToe = new TicTacToe();
+    }
+
     @Test
     public void verifyPlayerXgoesFirst() throws IllegalMoveException {
-        assertEquals(true, new TicTacToe().makeMark("X", 0, 0));
+        assertEquals(true, ticTacToe.makeMark("X", 0, 0));
     }
 
     @Test(expected = IllegalMoveException.class)
     public void verifyPlayerOGoingFirstThrowsException() throws IllegalMoveException {
-        assertEquals(true, new TicTacToe().makeMark("O", 0, 0));
+        assertEquals(true, ticTacToe.makeMark("O", 0, 0));
     }
 
 //    @Test(expected = InvalidMoveException.class)
@@ -20,15 +28,12 @@ public class TicTacToeShould {
 
     @Test
     public void ensurePlayersAlternateTurns() throws IllegalMoveException {
-        TicTacToe ticTacToe = new TicTacToe();
         assertEquals(true, ticTacToe.makeMark("X", 0, 0));
         assertEquals(true, ticTacToe.makeMark("O", 0, 0));
     }
 
     @Test
     public void allowPlayersToMarkAPosition() throws IllegalMoveException {
-        TicTacToe ticTacToe = new TicTacToe();
-
         ticTacToe.makeMark("X", 0, 0);
 
         assertEquals("X", ticTacToe.getMarkAtPosition(0, 0));
