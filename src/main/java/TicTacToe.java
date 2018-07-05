2ae8c9746a241dd4f4c8be341fd5f2700d9a5a7a
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-05 10:29:27 +0100

    Added test verifyPlayerXgoesFirst()

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.NotImplementedException;
+
+public class TicTacToe {
+
+
+    public boolean makeMark() {
+        throw new NotImplementedException("Not supported yet.");
+    }
+}
---
+++ src/test/java/TicTacToeShould.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicTacToeShould {
+
+    @Test
+    public void verifyPlayerXgoesFirst() {
+        assertEquals(true, new TicTacToe().makeMark());
+    }
+}
